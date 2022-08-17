4c2e845e157367ca15ed575c4a10c4c2ba7b4500
UrbanPistek <urban.pistek@gmail.com>
2022-08-16 20:02:24 -0600

    Added more to linux install script

--- linux_install.sh
+++ linux_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 apt () {
-	apt update # after running this we know shit is updated
-	apt install -y \
+	sudo apt update # after running this we know shit is updated
+	sudo apt install -y \
 		vim \
 		neovim \
 		curl \
@@ -13,20 +13,57 @@ apt () {
 		wget \
 		npm \
 		python3-pip \
-		python3-venv
+		python3-venv \
+		gparted \
+		minicom
 }
 
 snaps () {
-    snap install discord
-    snap install postman
-    snap install slack 
-	snap install spotify
-	snap install cura-slicer
-	snap install gimp
+    sudo snap install discord
+	sudo snap install postman
+	sudo snap install slack 
+	sudo snap install spotify
+	sudo snap install cura-slicer
+	sudo snap install gimp
+	sudo snap install bitwarden
+	sudo snap install zoom-client
+	sudo snap install vlc
 }
 
 docker () {
 	sudo bash ./scripts/linux_docker_setup.sh 
+}
+
+vscode () {
+	sudo bash ./scripts/vscode_install.sh
+}
+
+brave () {
+	sudo bash ./scripts/brave_install.sh
+}
+
+obsidian () {
+	wget https://github.com/obsidianmd/obsidian-releases/releases/download/v0.15.9/obsidian_0.15.9_amd64.deb
+	sudo dpkg -i ./obsidian_0.15.9_amd64.deb
+	rm obsidian_0.15.9_amd64.deb
+}
+
+zsh () {
+	cd $HOME
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+	echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+	exec zsh
+}
+
+neovim () {
+	sudo bash ./scripts/neovim_install.sh
+}
+
+dotfiles () {
+	# add cp ./.zshrc ~/
+	# add cp ./.vimrc ~/
+	# add cp ./.config/nvim ~/.config
 }
 
 echo "Setting up linux"
